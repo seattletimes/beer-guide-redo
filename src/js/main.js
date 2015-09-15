99d810ea01baa4e22676c8b729e73f0d24e13895
Audrey Carlsen <acarlsen@seattletimes.com>
2015-09-15 13:59:15 -0700

    new icons and MUCH MUCH MORE

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
-// require("./lib/social");
-// require("./lib/ads");
+require("./lib/social");
+require("./lib/ads");
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
 
 var dot = require("./lib/dot");
 var card = dot.compile(require("./_card.html"));
+
+var beerBox = document.querySelector(".beer-box");
 
 var groupedAbvs = {};
 abvData.forEach(function(a) {
@@ -21,38 +23,21 @@ for (var abv in groupedAbvs) {
   });
 }
 
-document.querySelector(".info-box").innerHTML = `
-  <div class="text-box">
-    <div class="title">What's in your beer?</div>
-    <div class="text">
-      The three main ingredients are water, malt (Barley grain that has been kiln dried) and hops (a cone-like fruit grown on vines). From the combination of these ingredients and the fermenting yeast used during brewing come the many colors, flavors and aromas of beer.
-    </div>
-  </div>
-
-  <div class="text-box">
-    <div class="title">Beers have style?</div>
-    <div class="text">
-      The most common beer styles are ales and lagers. When other ingredients are used to enhance the flavor such as wheat, pumpkin, honey, spices, fruit, smoked malt or chile peppers, the beer is known as a specialty beer.
-      <div class="spacer"></div>
-      The alcohol content of beer is measured in percent by volume (ABV). The low end is around 2 percent rising up to a sky-high 25 percent! Most beers have an ABV of 4 to 6 percent.
-    </div>
-  </div>
-
-  <div class="text-box">
-    <div class="title">Pint glass 101</div>
-    <div class="text">
-      Pint glasses come in different styles. English pint glasses include Nonic, Imperial and Tulip styles. The Becker is the German's pint. Both English and German pint glasses hold 20 ounces. The American pint (tumbler or conical-shaped) glass is the most common style and holds 16 ounces.
-    </div>
-  </div>
-`;
-
 Array.prototype.forEach.call(document.querySelectorAll('.dot'), function(dot) { 
   dot.addEventListener("click", function(e) {
     var beer = beerData.filter(function(row) {
       return row.short == e.target.getAttribute("data-type");
     }).pop();
-    document.querySelector(".info-box").innerHTML = card(beer);
+    beerBox.classList.add("visible");''
+    beerBox.innerHTML = card(beer);
     if (document.querySelector(".selected")) {document.querySelector(".selected").classList.remove("selected");}
     e.target.classList.add("selected");
   });
+});
+
+document.querySelector(".container").addEventListener("click", function(e) {
+  if (e.target.classList.contains("close")) {
+    beerBox.classList.remove("visible");
+    document.querySelector(".selected").classList.remove("selected");
+  }
 });
